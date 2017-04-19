9ae61c7aca6ec80f20610f41a471a925c5432bd1
Sayli Kadam <saylikadam16@gmail.com>
2017-04-19 19:12:25 +0530

    Show fragment on click of forecast and add back navigation

---
+++ app/src/main/java/com/twevent/xtrememobileweatherapp/DetailFragment.java
@@ -0,0 +1,30 @@
+package com.twevent.xtrememobileweatherapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class DetailFragment extends Fragment {
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        return view;
+    }
+
+
+}
