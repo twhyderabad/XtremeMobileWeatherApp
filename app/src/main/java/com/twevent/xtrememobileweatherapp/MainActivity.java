62f027b2971ddba0546f5f95ef883c7408ac5c97
Sayli Kadam <saylikadam16@gmail.com>
2017-04-19 17:40:07 +0530

    Create landing page of weather app

--- app/src/main/java/com/twevent/xtrememobileweatherapp/MainActivity.java
+++ app/src/main/java/com/twevent/xtrememobileweatherapp/MainActivity.java
@@ -4,26 +4,34 @@ import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.twevent.xtrememobileweatherapp.model.Weather;
+import com.twevent.xtrememobileweatherapp.presenter.WeatherPresenter;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private Weather currentWeather = null;
+    private Map<String, Integer> weatherStatusImageMap = new HashMap<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadWeatherFromFile();
         Toolbar toolbar = (Toolbar) findViewById(R.id.custom_toolbar);
         setSupportActionBar(toolbar);
+        setWeatherStatusImageMap();
+        loadWeatherFromFile();
     }
 
     private void loadWeatherFromFile() {
@@ -39,7 +47,27 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void setWeatherStatusImageMap() {
+        weatherStatusImageMap.put("light rain",R.drawable.rainy_main);
+        weatherStatusImageMap.put("moderate rain",R.drawable.sun);
+        weatherStatusImageMap.put("clear sky",R.drawable.sunny_main);
+    }
+
     private void renderWeather() {
-        // RENDER WEATHER
+        WeatherPresenter presenter = new WeatherPresenter(currentWeather);
+        TextView cityName = (TextView) findViewById(R.id.cityName);
+        cityName.setText(currentWeather.getName());
+
+        TextView date = (TextView) findViewById(R.id.date);
+        date.setText(presenter.getDayOfTheWeek());
+
+        TextView temperature = (TextView) findViewById(R.id.temperature);
+        temperature.setText(presenter.getTemperatureInCelsius());
+
+        TextView weatherDescription = (TextView) findViewById(R.id.weatherDescription);
+        weatherDescription.setText(currentWeather.getWeather().get(0).getDescription());
+
+        ImageView weatherImage = (ImageView) findViewById(R.id.weatherImage);
+        weatherImage.setImageResource(weatherStatusImageMap.get(currentWeather.getWeather().get(0).getDescription()));
     }
 }
