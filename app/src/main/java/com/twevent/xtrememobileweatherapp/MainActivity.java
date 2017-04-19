bbdeb790a9593ab59094079336f107bd533fddcd
Sayli Kadam <saylikadam16@gmail.com>
2017-04-19 16:39:44 +0530

    add resources required for showing landing page

--- app/src/main/java/com/twevent/xtrememobileweatherapp/MainActivity.java
+++ app/src/main/java/com/twevent/xtrememobileweatherapp/MainActivity.java
@@ -1,13 +1,43 @@
 package com.twevent.xtrememobileweatherapp;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.google.gson.Gson;
+import com.twevent.xtrememobileweatherapp.model.Weather;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Weather currentWeather = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadWeatherFromFile();
+    }
+
+    private void loadWeatherFromFile() {
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open("cityData.json");
+            Gson gson = new Gson();
+            Reader inputReader = new InputStreamReader(inputStream);
+            currentWeather = gson.fromJson(inputReader, Weather.class);
+            renderWeather();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void renderWeather() {
+        // RENDER WEATHER
     }
 }
