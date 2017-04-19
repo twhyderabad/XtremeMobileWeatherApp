35eebc74e2570a2d97afc48efe09839ebbadd338
Sayli Kadam <saylikadam16@gmail.com>
2017-04-19 16:39:44 +0530

    add resources required for showing landing page

---
+++ app/src/main/java/com/twevent/xtrememobileweatherapp/model/Weather.java
@@ -0,0 +1,121 @@
+package com.twevent.xtrememobileweatherapp.model;
+
+import java.util.List;
+
+public class Weather {
+
+    private String name;
+    private int visibility;
+    private List<WeatherDetails> weather;
+    private Temperature main;
+    private Wind wind;
+    private Sys sys;
+
+    private String dt;
+
+    public String getDatetime() {
+        return dt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<WeatherDetails> getWeather() {
+        return weather;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public Temperature getMain() {
+        return main;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public Sys getSys() {
+        return sys;
+    }
+
+
+    public class WeatherDetails {
+        private String description;
+        private String icon;
+        private int id;
+        private String main;
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getMain() {
+            return main;
+        }
+    }
+
+    public class Temperature {
+        private float humidity;
+        private float pressure;
+        private float temp;
+        private float temp_max;
+        private float temp_min;
+
+        public float getHumidity() {
+            return humidity;
+        }
+
+        public float getPressure() {
+            return pressure;
+        }
+
+        public float getTemp() {
+            return temp;
+        }
+
+        public float getTemp_max() {
+            return temp_max;
+        }
+
+        public float getTemp_min() {
+            return temp_min;
+        }
+    }
+
+    public class Wind {
+        private float deg;
+        private float speed;
+
+        public float getDeg() {
+            return deg;
+        }
+
+        public float getSpeed() {
+            return speed;
+        }
+    }
+
+    public class Sys {
+        private long sunrise;
+        private long sunset;
+
+        public long getSunrise() {
+            return sunrise;
+        }
+
+        public long getSunset() {
+            return sunset;
+        }
+    }
+}
---
+++ app/src/main/java/com/twevent/xtrememobileweatherapp/presenter/WeatherPresenter.java
@@ -0,0 +1,38 @@
+package com.twevent.xtrememobileweatherapp.presenter;
+
+
+import com.twevent.xtrememobileweatherapp.model.Weather;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class WeatherPresenter {
+    private List<String> daysInAWeek;
+    private Weather weather;
+
+    public WeatherPresenter(Weather weather) {
+        super();
+        this.weather = weather;
+        daysInAWeek = Arrays.asList("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+    }
+
+    public String getTemperatureInCelsius() {
+        return String.valueOf(weather.getMain().getTemp()) + "\u00b0" + "C";
+    }
+
+    public String getDayOfTheWeek() {
+
+        long unixSeconds = Long.parseLong(weather.getDatetime());
+        Date date = new Date(unixSeconds * 1000L);
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+
+        return c.get(Calendar.DAY_OF_MONTH) + "-" +
+        c.get(Calendar.MONTH) + "-" + c.get(Calendar.YEAR) + ", " +
+        daysInAWeek.get(dayOfWeek - 1);
+    }
+
+}
