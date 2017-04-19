eeadfd54431dfbe3182cae047ad04871e15f1682
Sayli Kadam <saylikadam16@gmail.com>
2017-04-19 20:28:38 +0530

    Add AsynTask to fetch data from API Call

---
+++ app/src/main/java/com/twevent/xtrememobileweatherapp/tasks/AsyncWeatherForecastTask.java
@@ -0,0 +1,50 @@
+package com.twevent.xtrememobileweatherapp.tasks;
+
+import android.os.AsyncTask;
+
+import com.twevent.xtrememobileweatherapp.WeatherResponseListener;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class AsyncWeatherForecastTask extends AsyncTask<String, Void, String> {
+
+	private static final String FAILED_EVENT = "FailedEvent";
+	private WeatherResponseListener listener;
+
+	public AsyncWeatherForecastTask(WeatherResponseListener listener) {
+		super();
+		this.listener = listener;
+	}
+
+	@Override
+	protected void onPostExecute(String o) {
+		super.onPostExecute(o);
+		if(FAILED_EVENT.equals(o)) {
+			listener.weatherForecastFailed();
+		} else {
+			listener.weatherForecastReceived(o);
+		}
+	}
+
+	@Override
+	protected String doInBackground(String... urls) {
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder().url(urls[0]).build();
+		Call newCall = client.newCall(request);
+		try {
+			Response response = newCall.execute();
+			if (response.isSuccessful()) {
+				return response.body().string();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return FAILED_EVENT;
+	}
+}
